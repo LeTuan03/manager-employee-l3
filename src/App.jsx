4f18596d860970f925a3a3bf159971e108da681b
LeTuan03 <leetuan191003@gmail.com>
2023-07-29 14:27:32 +0700

    fix login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LayoutHomePage from "./components/layout/LayoutHomePage";
 import Employee from "./pages/manager/Employee";
 import { getAccount } from "./services/api";
@@ -17,71 +14,66 @@ import AwaitingApproval from "./pages/awaiting-approval/AwaitingApproval";
 import Approved from "./pages/approved/Approved";
 import { STATUS } from "./constants/constants";
 export default function App() {
-  const dispatch = useDispatch();
-  const { isAuthenticated } = useSelector((state) => state.account);
-  const getRoleAccount = async () => {
-      if (window.location.pathname === "/login") {
-        return
-      }
-      const res = await getAccount()
-      if (res?.status === STATUS.SUCCESS) {
-        dispatch(doLoginAction(res?.data[0]));
-      }
-  }
-  useEffect(() => {
-    getRoleAccount()
-  }, [])
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <LayoutHomePage></LayoutHomePage>,
-      errorElement: <>Not Found</>,
-      children: [
-        {
-          index: true,
-          element: <></>,
-        },
-        {
-          path: "/manage_employee",
-          element: (
-            <Employee></Employee>
-          ),
-        },
-        {
-          path: "/release",
-          element: <PageEnd></PageEnd>,
-        },
-        {
-          path: "/awaiting_approval",
-          element: <AwaitingApproval></AwaitingApproval>,
-        },
-        {
-          path: "/approved",
-          element: <Approved></Approved>,
+    const dispatch = useDispatch();
+    const { isAuthenticated } = useSelector((state) => state.account);
+    const getRoleAccount = async () => {
+        // if (window.location.pathname === "/login") {
+        //   return
+        // }
+        const res = await getAccount();
+        if (res?.status === STATUS.SUCCESS) {
+            dispatch(doLoginAction(res?.data[0]));
         }
-        ,
+    };
+    useEffect(() => {
+        getRoleAccount();
+    }, []);
+    const router = createBrowserRouter([
         {
-          path: "/addnew_employee",
-          element: (
-            <RoleUserRoute role={4}>
-              <AddUserPage></AddUserPage>
-            </RoleUserRoute>
-          ),
-        }
-      ],
-    },
-    {
-      path: "/login",
-      element: <Signin></Signin>,
-    }
-  ]);
-  return (
-    <>
-      
-        {/* isAuthenticated ||
+            path: "/",
+            element: <LayoutHomePage></LayoutHomePage>,
+            errorElement: <>Not Found</>,
+            children: [
+                {
+                    index: true,
+                    element: <></>,
+                },
+                {
+                    path: "/manage_employee",
+                    element: <Employee></Employee>,
+                },
+                {
+                    path: "/release",
+                    element: <PageEnd></PageEnd>,
+                },
+                {
+                    path: "/awaiting_approval",
+                    element: <AwaitingApproval></AwaitingApproval>,
+                },
+                {
+                    path: "/approved",
+                    element: <Approved></Approved>,
+                },
+                {
+                    path: "/addnew_employee",
+                    element: (
+                        <RoleUserRoute role={4}>
+                            <AddUserPage></AddUserPage>
+                        </RoleUserRoute>
+                    ),
+                },
+            ],
+        },
+        {
+            path: "/login",
+            element: <Signin></Signin>,
+        },
+    ]);
+    return (
+        <>
+            {/* isAuthenticated ||
           window.location.pathname === "/login" ? <RouterProvider router={router} /> : <>Loading...</> */}
-          <RouterProvider router={router} />
-      
-    </>
-  );
+            <RouterProvider router={router} />
+        </>
+    );
 }
