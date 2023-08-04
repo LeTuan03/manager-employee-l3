8db5cce1389beb60fd24627cfecc3ddd4a947566
LeTuan03 <leetuan191003@gmail.com>
2023-08-04 14:24:46 +0700

    check code

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -1,27 +1,30 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_BACKEND_URL
+const baseURL = import.meta.env.VITE_BACKEND_URL;
 const instance = axios.create({
     baseURL,
     // headers: {
     //     "Content-Type": "application/json"
     // }
 });
-instance.interceptors.request.use(function (config) {
-    const jwt = localStorage.getItem("access_token")
-    if (config.url !== "/oauth/token") {
-        const token = `Bearer ${jwt}`
-        config.headers.Authorization = token;
+instance.interceptors.request.use(
+    function (config) {
+        const jwt = localStorage.getItem("access_token");
+        if (config.url !== "/oauth/token") {
+            const token = `Bearer ${jwt}`;
+            config.headers.Authorization = token;
+        }
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
     }
-    return config;
-}, function (error) {
-    return Promise.reject(error);
-});
-instance.interceptors.response.use(function (response) {
-    return response; 
-}, function (error) {
-    if (+error?.response?.status === 401) {
-        window.location.href = '/login'
+);
+instance.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-});
-export default instance
+);
+export default instance;
